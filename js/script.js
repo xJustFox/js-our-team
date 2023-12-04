5248f1226571ed573f4fad158b850a626281d522
xJustFox <dodydaversa@gmail.com>
2023-12-04 13:33:23 +0100

    [Upgrade] Created array peoplesTeam

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let peoplesTeam = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrda",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    } 
+]
