c64061ec95039d544f58560de10879d7384e6d3d
xJustFox <dodydaversa@gmail.com>
2023-12-04 14:13:20 +0100

    [Upgrade] Exercise completed with Bonuses

--- js/script.js
+++ js/script.js
@@ -1,37 +1,44 @@
 let peoplesTeam = [
     {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        img: "wayne-barnett-founder-ceo.jpg"
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: '<img src="./imgs/wayne-barnett-founder-ceo.jpg" alt="">'
     },
     {
-        name: "Angela Caroll",
-        role: "Chief Editor",
-        img: "angela-caroll-chief-editor.jpg"
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: '<img src="./imgs/angela-caroll-chief-editor.jpg" alt="">'
     },
     {
-        name: "Walter Gordon",
-        role: "Office Manager",
-        img: "walter-gordon-office-manager.jpg"
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        img: '<img src="./imgs/walter-gordon-office-manager.jpg" alt="">'
     },
     {
-        name: "Angela Lopez",
-        role: "Social Media Manager",
-        img: "angela-lopez-social-media-manager.jpg"
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: '<img src="./imgs/angela-lopez-social-media-manager.jpg" alt="">'
     },
     {
-        name: "Scott Estrda",
-        role: "Developer",
-        img: "scott-estrada-developer.jpg"
+        name: 'Scott Estrda',
+        role: 'Developer',
+        img: '<img src="./imgs/scott-estrada-developer.jpg" alt="">'
     },
     {
-        name: "Barbara Ramos",
-        role: "Graphic Designer",
-        img: "barbara-ramos-graphic-designer.jpg"
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: '<img src="./imgs/barbara-ramos-graphic-designer.jpg" alt="">'
     } 
 ]
 
 for (let i = 0; i < peoplesTeam.length; i++) {
     console.log(peoplesTeam[i]);
-    
+    let peopleData = peoplesTeam[i];
+
+    let listPeoples = document.getElementById("list-people");
+    let col = document.createElement("div");
+    col.classList.add("col-4", "p-4", "text-center");
+
+    col.innerHTML = `${peopleData.img} <h3>${peopleData.name}</h3>  <p>${peopleData.role}</p>`;
+    listPeoples.appendChild(col);
 }
