cbc14aadefbf318dca7c52581219287c5cde6911
xJustFox <dodydaversa@gmail.com>
2023-12-05 10:04:28 +0100

    [Upgrade] Correct exercise

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@ let peoplesTeam = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        img: '<img src="./imgs/wayne-barnett-founder-ceo.jpg" alt="">'
+        img: './imgs/wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        img: '<img src="./imgs/angela-caroll-chief-editor.jpg" alt="">'
+        img: './imgs/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        img: '<img src="./imgs/walter-gordon-office-manager.jpg" alt="">'
+        img: './imgs/walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        img: '<img src="./imgs/angela-lopez-social-media-manager.jpg" alt="">'
+        img: './imgs/angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrda',
         role: 'Developer',
-        img: '<img src="./imgs/scott-estrada-developer.jpg" alt="">'
+        img: './imgs/scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        img: '<img src="./imgs/barbara-ramos-graphic-designer.jpg" alt="">'
+        img: './imgs/barbara-ramos-graphic-designer.jpg'
     } 
 ]
 
@@ -35,10 +35,10 @@ for (let i = 0; i < peoplesTeam.length; i++) {
     console.log(peoplesTeam[i]);
     let peopleData = peoplesTeam[i];
 
-    let listPeoples = document.getElementById("list-people");
+    const listPeoples = document.getElementById("list-people");
     let col = document.createElement("div");
     col.classList.add("col-4", "p-4", "text-center");
 
-    col.innerHTML = `${peopleData.img} <h3>${peopleData.name}</h3>  <p>${peopleData.role}</p>`;
+    col.innerHTML = `<img src="${peopleData.img}" alt=""> <h3>${peopleData.name}</h3>  <p>${peopleData.role}</p>`;
     listPeoples.appendChild(col);
 }
